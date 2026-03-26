e64f173bde7de35b795393f5e10cd071ae4d3db6
Martin Pokorny <89339813+mPokornyETM@users.noreply.github.com>
2026-03-26 23:34:14 +0100

    feat: enhance panel file support by adding XML handling and refactoring project retrieval logic (#50)

--- src/converter.ts
+++ src/converter.ts
@@ -14,12 +14,8 @@ import * as path from 'path';
 import * as os from 'os';
 import { pnlToXml, xmlToPnl } from '@winccoa-tools-pack/npm-winccoa-ui-pnl-xml';
 import { ExtensionOutputChannel } from './extensionOutput';
-import { getSelectedProject } from './otherExtensions';
-import {
-    ProjEnvProject,
-    ProjEnvProjectFileSysStruct,
-    getProjectByProjectPath,
-} from '@winccoa-tools-pack/npm-winccoa-core';
+import { getWinccoaProject } from './utils';
+import { ProjEnvProjectFileSysStruct } from '@winccoa-tools-pack/npm-winccoa-core';
 
 /** Result of a conversion operation */
 export interface ConversionResult {
@@ -42,39 +38,6 @@ export async function isEncryptedPanel(filePath: string): Promise<boolean> {
     } catch {
         return false;
     }
-}
-
-async function getWinccoaProject(oaPanelPath: string): Promise<ProjEnvProject | undefined> {
-    let currentProject: ProjEnvProject | undefined = await getProjectByProjectPath(oaPanelPath);
-
-    if (currentProject) {
-        return currentProject;
-    }
-
-    currentProject = (await getSelectedProject()) ?? undefined;
-
-    if (!currentProject) {
-        vscode.window.showWarningMessage(
-            `No WinCC OA project selected; cannot determine WinCC OA version for conversion for given panel ${oaPanelPath}.\nPlease select a runnable project and try again.`,
-        );
-        return undefined;
-    }
-
-    if (!currentProject.getVersion()) {
-        vscode.window.showWarningMessage(
-            `Selected project "${currentProject.getName()}" does not have a version; cannot determine WinCC OA version for conversion for given panel ${oaPanelPath}.\nPlease select a runnable project with a version and try again.`,
-        );
-        return undefined;
-    }
-
-    if (!currentProject.getConfigPath()) {
-        vscode.window.showWarningMessage(
-            `Selected project "${currentProject.getName()}" does not have a config file; cannot determine WinCC OA version for conversion for given panel ${oaPanelPath}.\nPlease select a runnable project with a config file and try again.`,
-        );
-        return undefined;
-    }
-
-    return currentProject;
 }
 
 /**
